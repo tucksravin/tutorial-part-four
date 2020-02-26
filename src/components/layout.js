4059e8a7ded5c028f5c276d8e057dad1c5041c47
Tucker Lemos <tucksravine1@gmail.com>
2020-02-26 13:20:42 -0800

    grey

--- src/components/layout.js
+++ src/components/layout.js
@@ -17,34 +17,35 @@ export default ({ children }) => {
     `
   )
   return (
-  <div
-    css={css`
-      margin: 0 auto;
-      max-width: 700px;
-      padding: ${rhythm(2)};
-      padding-top: ${rhythm(1.5)};
-    `}
-  >
-    <Link to={`/`}>
-      <h3
-        css={css`
-          margin-bottom: ${rhythm(2)};
-          display: inline-block;
-          font-style: normal;
-        `}
-      >
-           {data.site.siteMetadata.title}
-      </h3>
-    </Link>
-    <Link
-      to={`/about/`}
+    <div
       css={css`
-        float: right;
+        margin: 0 auto;
+        max-width: 700px;
+        padding: ${rhythm(2)};
+        padding-top: ${rhythm(1.5)};
+        background-color: grey;
       `}
     >
-      About
-    </Link>
-    {children}
-  </div>
-)
+      <Link to={`/`}>
+        <h3
+          css={css`
+            margin-bottom: ${rhythm(2)};
+            display: inline-block;
+            font-style: normal;
+          `}
+        >
+          {data.site.siteMetadata.title}
+        </h3>
+      </Link>
+      <Link
+        to={`/about/`}
+        css={css`
+          float: right;
+        `}
+      >
+        About
+      </Link>
+      {children}
+    </div>
+  )
 }
